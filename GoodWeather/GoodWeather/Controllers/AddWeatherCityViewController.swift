52a3c25948435b817455033b3674e56b5180e9d9
Yusuf Ansar <yusuf.ansar@happay.in>
2019-02-10 21:43:05 +0530

    Added required custom view controller & binded it to corresponding view controller scenes in storyboard

---
+++ GoodWeather/GoodWeather/Controllers/AddWeatherCityViewController.swift
@@ -0,0 +1,13 @@
+//
+//  AddWeatherCityViewController.swift
+//  GoodWeather
+//
+//  Created by Yusuf Ansar on 10/02/19.
+//  Copyright © 2019 Yusuf Ansar. All rights reserved.
+//
+
+import UIKit
+
+class AddWeatherCityViewController: UIViewController {
+    
+}
---
+++ GoodWeather/GoodWeather/Controllers/WeatherListTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  WeatherListTableViewController.swift
+//  GoodWeather
+//
+//  Created by Yusuf Ansar on 10/02/19.
+//  Copyright © 2019 Yusuf Ansar. All rights reserved.
+//
+
+import UIKit
+
+class WeatherListTableViewController: UITableViewController {
+    
+}
