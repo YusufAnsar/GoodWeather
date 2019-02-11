895e97441004d3025e4f2099e7b827cf61233aab
Yusuf Ansar <yusuf.ansar@happay.in>
2019-02-11 06:34:27 +0530

    Added custom Cell to display weather information

---
+++ GoodWeather/GoodWeather/Custom Cells/WeatherTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  WeatherTableViewCell.swift
+//  GoodWeather
+//
+//  Created by Yusuf Ansar on 11/02/19.
+//  Copyright © 2019 Yusuf Ansar. All rights reserved.
+//
+
+import UIKit
+
+class WeatherTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var cityNameLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+
+}
